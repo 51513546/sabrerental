20cf1ee3787960961f9295c90c587b27c6d097c8
unknown <prasantaroutray8@gmail.com>
2020-02-10 01:21:42 +0530

    my commit first

---
+++ src/main/java/com/infogain/stacksimplify/SabrerentalApplication.java
@@ -0,0 +1,13 @@
+package com.infogain.stacksimplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabrerentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabrerentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infogain/stacksimplify/SabrerentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infogain.stacksimplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabrerentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
