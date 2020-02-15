41e4a5c3649ea1630ce65311f5b7953ecb8c9814
home <prasantaroutray8@gmail.com>
2020-02-15 19:15:11 +0530

    Committing for order

--- src/main/java/com/infogain/stacksimplify/repository/OrderRepository.java
+++ src/main/java/com/infogain/stacksimplify/repository/OrderRepository.java
@@ -1,9 +1,8 @@
 package com.infogain.stacksimplify.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.infogain.stacksimplify.entities.Order;
-import com.infogain.stacksimplify.entities.User;
+
 
 @Repository
 public interface OrderRepository  extends JpaRepository<Order, Long>{
